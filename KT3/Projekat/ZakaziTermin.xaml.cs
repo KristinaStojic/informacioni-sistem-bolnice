0f22681765e4fc1fed5490ede4d767b3059472ca
sanjadrinic <sanja.drinic776@gmail.com>
2021-04-05 23:20:41 +0200

    update: GUI za zakazivanje termina

--- KT3/Projekat/ZakaziTermin.xaml.cs
+++ KT3/Projekat/ZakaziTermin.xaml.cs
@@ -16,19 +16,20 @@ using static Model.Termin;
 
 namespace Projekat
 {
-    /// <summary>
-    /// Interaction logic for ZakaziTermin.xaml
-    /// </summary>
+    // TODO
     public partial class ZakaziTermin : Window
     {
+        public Lekar lekarr;
         public ZakaziTermin()
         {
             InitializeComponent();
-            dp.BlackoutDates.AddDatesInPast();
-            CalendarDateRange cdr = new CalendarDateRange();
-            cdr.Start = DateTime.Now.AddDays(3);
-            cdr.End = DateTime.Now.AddDays(2000);
-            dp.BlackoutDates.Add(cdr);
+            datum.BlackoutDates.AddDatesInPast();
+            //TODO: lekarr je izabrani lekar prijavljenog pacijenta
+            //lekarr = 
+
+            this.dgSearch.ItemsSource = MainWindow.lekari;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(dgSearch.ItemsSource);
+            view.Filter = UserFilter;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -44,16 +45,14 @@ namespace Projekat
             {
                 int brojTermina = TerminMenadzer.GenerisanjeIdTermina();
                 String formatted = null;
-                DateTime? selectedDate = dp.SelectedDate;
+                DateTime? selectedDate = datum.SelectedDate;
                 Console.WriteLine(selectedDate);
                 if (selectedDate.HasValue)
                 {
                     formatted = selectedDate.Value.ToString("MM/dd/yyyy", System.Globalization.CultureInfo.InvariantCulture);
 
                 }
-                String vp = text2.Text;
-                //String vk = text3.Text;
-                //TODO
+                String vp = vpp.Text;
                 String vk;
                 String hh = vp.Substring(0, 2);
                 String min = vp.Substring(3);
@@ -83,10 +82,26 @@ namespace Projekat
                 {
                     tp = TipTermina.Operacija;
                 }
-                int idLek = int.Parse(text4.Text);
-                Lekar l = new Lekar(idLek, "Filip", "Filipovic");
+                Termin s = new Termin(brojTermina, formatted, vp, vk, tp);
+                try
+                {
+                    if (dgSearch.SelectedItems.Count > 0)
+                    {
+                        Lekar selLekar = (Lekar)dgSearch.SelectedItem;
+                        s.Lekar = selLekar;
+                    }
+                } catch (Exception)
+                {
+                    MessageBox.Show("Izaberite lekara kod kog želite da zakažete termin", "Greška", MessageBoxButton.OK);
+                }
+                    
+                /*else
+                {
+                    // TODO: optimizovati!
+                    s.Lekar = lekarr;
+                }*/
 
-                Termin s = new Termin(brojTermina, formatted, vp, vk, tp, l);
+
                 foreach (Pacijent p in PacijentiMenadzer.pacijenti)
                 {
                     if (p.IdPacijenta == 1)
@@ -94,9 +109,13 @@ namespace Projekat
                         s.Pacijent = p;
                     }
                 }
-                foreach (Sala sala in SaleMenadzer.NadjiSveSale())
+                foreach (Sala sala in SaleMenadzer.sale)
                 {
-                    try
+                    if (sala.Status.Equals(status.Slobodna))
+                    {
+                        s.Prostorija = sala;
+                    }
+                    /*try
                     {
                         if (sala.Status.Equals(status.Slobodna))
                         {
@@ -107,7 +126,8 @@ namespace Projekat
                             { 
                                 if (PrikaziSalu.Sale[i].Id == sala.Id)
                                 {
-                                    PrikaziSalu.Sale[i].Status = status.Zauzeta;
+
+                                    //PrikaziSalu.Sale[i].Status = status.Zauzeta;
                                 }
                             }
                             break;
@@ -115,15 +135,15 @@ namespace Projekat
                     }
                     catch (Exception)
                     {
-                        MessageBox.Show("Ne postoji nijedna slobodna sala", "Zauzete sale");
-                    }
+                        MessageBox.Show("Ne postoji nijedna slobodna sala", "Greška", MessageBoxButton.OK);
+                    }*/
                 }
                 TerminMenadzer.ZakaziTermin(s);
                 this.Close();
 
             } catch(System.Exception)
             {
-                MessageBox.Show("Niste uneli ispravne podatke", "Greška", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Niste uneli sve podatke", "Greška", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             
         }
@@ -131,6 +151,52 @@ namespace Projekat
         private void zdravstevniKarton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // elektronsko placanje
+        }
+
+        private void odustani_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(txtFilter.Text))
+                return true;
+            else
+                return ((item as Lekar).PrezimeLek.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(dgSearch.ItemsSource).Refresh();
+        }
+
+        private void dgSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgSearch.SelectedItems.Count > 0)
+            {
+                Lekar item = (Lekar)dgSearch.SelectedItems[0];
+                imePrz.Text = item.ToString();
+            }
+        }
+
+        private void imePrz_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void vpp_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void preferenca_Click(object sender, RoutedEventArgs e)
+        {
+           // prozor za odabir lekara po preferenci
         }
     }
 }
