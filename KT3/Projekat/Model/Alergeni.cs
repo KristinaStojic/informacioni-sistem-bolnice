d41fdaccbc79277f5679543367784c4e04e059a0
Kristina Stojic <kristinastojic99@uns.ac.rs>
2021-04-20 01:13:08 +0200

    Lekar: napravljena klasa alergeni

---
+++ KT3/Projekat/Model/Alergeni.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projekat.Model
+{
+    public class Alergeni
+    {
+        public int IdAlergena { get; set; }
+        public int IdPacijenta { get; set; }
+        public string NazivLeka { get; set; }
+        public string SifraLeka { get; set; }
+        public string NuspojavaNaLek { get; set; }
+        public string VremeReakcije { get; set; }
+
+        public Alergeni() { }
+
+        public Alergeni(int idAlergena, int idPacijenta, string nazivLeka, string sifraLeka, string nuspojava, string vremeReakcije)
+        {
+            this.IdAlergena = idAlergena;
+            this.IdPacijenta = idPacijenta;
+            this.NazivLeka = nazivLeka;
+            this.SifraLeka = sifraLeka;
+            this.NuspojavaNaLek = nuspojava;
+            this.VremeReakcije = vremeReakcije;
+        }
+    }
+}
