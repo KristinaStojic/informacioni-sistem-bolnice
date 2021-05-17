7c49c154aa8c31ff6c8d45cef2c09a62cca37d62
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2021-05-18 01:39:35 +0200

    sekretar-ispravka

--- KT3/Projekat/PrikaziTermin.xaml.cs
+++ KT3/Projekat/PrikaziTermin.xaml.cs
@@ -18,7 +18,6 @@ using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows;
 using Projekat.Lokalizacija;
-using Projekat.Servis;
 
 namespace Projekat
 {
--- KT3/Projekat/ZakazaniTerminiPacijent.xaml.cs
+++ KT3/Projekat/ZakazaniTerminiPacijent.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Model;
 using Projekat.Model;
-using Projekat.Servis;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
