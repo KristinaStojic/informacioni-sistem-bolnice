ec8eb405181c772d437ee9641cff1b61d558913e
Kina999 <73175451+Kina999@users.noreply.github.com>
2021-05-18 10:48:59 +0200

    brisanje lijeka

--- KT3/Projekat/ViewModel/BrisanjeLijeka.cs
+++ KT3/Projekat/ViewModel/BrisanjeLijeka.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Projekat.Servis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Projekat.ViewModel
 {
-    class BrisanjeLijeka
+    
+    class BrisanjeLijeka : ObservableObject
     {
+        public RelayCommand ObrisiLijekCommand { get; private set; }
+        public BrisanjeLijeka()
+        {
+            ObrisiLijekCommand = new RelayCommand(ObrisiLijek);
+        }
+        private void ObrisiLijek()
+        {
+            LekoviServis.obrisiLijek(izabraniLijek);
+            Lijekovi.Lekovi.Remove(izabraniLijek);
+            this.Close();
+        }
     }
+
+   
 }
