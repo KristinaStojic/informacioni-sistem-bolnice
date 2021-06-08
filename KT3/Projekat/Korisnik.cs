ad9acf19ab798f163d086800d92083506a3fc42e
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2021-06-08 23:38:04 +0200

    izmene

---
+++ KT3/Projekat/Korisnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Projekat.Model;
+
+namespace Projekat
+{
+    public class Korisnik: IObserver
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public long Jmbg { get; set; }
+        public long BrojTelefona { get; set; }
+        public string Email { get; set; }
+        public string AdresaStanovanja { get; set; }
+
+        public List<Obavestenja> obavestenjaKorisnika { get; set; } = new List<Obavestenja>();
+
+        public Korisnik() {}
+
+        public void Update(ISubject subject)
+        {
+            ObjavaObavestenja o = subject as ObjavaObavestenja;
+            if (o != null)
+            {
+                obavestenjaKorisnika.Add(o.NovoObavestenje);
+                Console.WriteLine("dodato obavestenje");
+            }
+        }
+    }
+}
---
+++ KT3/Projekat/Model/Observable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projekat.Model
+{
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Notify();
+    }
+
+    public class ObjavaObavestenja : ISubject
+    {
+        private List<IObserver> observers;
+        private Obavestenja novoObavestenje;
+        public Obavestenja NovoObavestenje
+        {
+            get { return novoObavestenje; }
+            set
+            {
+                novoObavestenje = value;
+                Notify();
+            }
+        }
+
+        public ObjavaObavestenja()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.Update(this);
+            }
+        }
+    }
+
+    public interface IObserver
+    {
+        void Update(ISubject subject);
+    }
+}
