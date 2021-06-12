5e734e386d0fc6c63476227c632610f671cb4c70
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2021-06-12 22:28:29 +0200

    strategy pattern - renovacija sale

---
+++ KT3/Projekat/Servis/RenovacijaSpajanjeSaleServis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+using Projekat.Model;
+
+namespace Projekat.Servis
+{
+    public class RenovacijaSpajanjeSaleServis : IRenovacijaServis
+    {
+        public void Renoviraj(Renovacija renovacija)
+        {
+            SaleServis.dodajOpremuIzSaleZaDodavanje(renovacija.IzabranaSala, renovacija.NovaSala);
+            SaleServis.ObrisiSalu(renovacija.NovaSala);
+            SaleServis.sacuvajIzmjene();
+            SaleServis.zauzmiSalu(renovacija.Zauzece, renovacija.IzabranaSala);
+            SaleServis.zauzmiSalu(renovacija.Zauzece, renovacija.NovaSala);
+            SaleServis.sacuvajIzmjene();
+        }
+    }
+}
